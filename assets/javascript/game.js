b1b285b53b9e6911fca0a774a0502517830e6a30
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2017-11-28 21:23:10 -0500

    Hangman files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,114 @@
 var wins = 0;
-var numGuesses = 20;
-var wordChoices = ["candyman", "john carpenter", "elm street", "seven days", "xenomorph"];
+var numGuesses = 15;
+var wordChoices = [
+	["c", "a", "n", "d", "y", "m", "a", "n"], 
+	["c", "a", "r", "p", "e", "n", "t", "e", "r"], 
+	["k", "r", "u", "e", "g", "e", "r"], 
+	["x", "e", "n", "o", "m", "o", "r", "p", "h"]
+	];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var lettersAlreadyGuessed = [];
 var wordComputerChooses = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 
+/* creating underscore array on page */
+var underscores = [];
+for(var i = 0; i < wordComputerChooses.length; i++){
+    	underscores.push("_");	
+    };
 
+
+/* on key up function */ 
 document.onkeyup = function(event) {
-	var letter = String.fromCharCode(event.keyCode).toLowerCase();
+	/* basic set up */
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-	var winsHtml = document.getElementById('winNumber');
-    winsHtml.textContent = wins;
-
-    var guessesLeft = document.getElementById('guessesRemaining');
-    guessesLeft.textContent = numGuesses;
-    numGuesses--;
-
-    var hangman = document.getElementById('word');
-
-    hangman.textContent = wordComputerChooses;
+    underscores.join(" ");
 
     var userGuess = event.key;
 
-    lettersAlreadyGuessed.push(userGuess);
+    var winsHtml = document.getElementById('winNumber');
+    winsHtml.textContent = wins;
 
-    var guessedLetters = document.getElementById('lettersGuessed');
-    guessedLetters.textContent = lettersAlreadyGuessed;  
+    var wordUnderscores = document.getElementById('word');
+    wordUnderscores.textContent = underscores;  
 
-    /*
-    if (numGuesses < 1){
-	var lettersAlreadyGuessed = [];
-	var numGuesses = 20;
-	var wordComputerChooses = computerChoices[Math.floor(Math.random() * wordChoices.length)];
-	}; 
-	*/ 
+    /* making sure an alphabet key is pressed and that you can't press the same letter twice */
+    if (alphabet.indexOf(event.key) > -1 && lettersAlreadyGuessed.indexOf(event.key) < 0){
+
+    	lettersAlreadyGuessed.push(userGuess);
+
+    	var guessedLetters = document.getElementById('lettersGuessed');
+    	guessedLetters.textContent = lettersAlreadyGuessed;  
+
+    	numGuesses--;
+    	var guessesLeft = document.getElementById('guessesRemaining');
+    	guessesLeft.textContent = numGuesses;
+    } else {
+    	console.log("Please press a valid key!");
+    };
+
+ 	/*putting correct guesses into underscore array */
+    if (wordComputerChooses.indexOf(userGuess) > -1){
+    	for(var i = 0; i < wordComputerChooses.length; i++){
+    		if (wordComputerChooses[i] == userGuess){
+    			underscores[i] = userGuess;
+    			var wordUnderscores = document.getElementById('word');
+    			wordUnderscores.textContent = underscores;  
+    		};
+    	};
+    };
+    
+    if (underscores.join('') == wordComputerChooses.join('') && numGuesses > 1){
+    	alert("You won... :)");
+    	winning();
+    } else if (numGuesses < 1){
+    	alert("You lose! :(");
+    	losing();
+    };
 };
 
-
-
-
- /*
-function (winning){
+function winning(){
+	wins++;
 	var winsHtml = document.getElementById('winNumber');
 	winsHtml.textContent = wins;
-	var lettersAlreadyGuessed = [];
-	var numGuesses = 20;
-	var wordComputerChooses = computerChoices[Math.floor(Math.random() * wordChoices.length)];
+
+	lettersAlreadyGuessed = [];
+	var guessedLetters = document.getElementById('lettersGuessed');
+    guessedLetters.textContent = lettersAlreadyGuessed;  
+
+	numGuesses = 15;
+	var guessesLeft = document.getElementById('guessesRemaining');
+    guessesLeft.textContent = numGuesses;
+
+	wordComputerChooses = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+
+	underscores = [];
+	for(var i = 0; i < wordComputerChooses.length; i++){
+    	underscores.push("_");
+    };
+	var wordUnderscores = document.getElementById('word');
+    wordUnderscores.textContent = underscores;  
 };
 
-function (losing){
-	var lettersAlreadyGuessed = [];
-	var numGuesses = 20;
-	var wordComputerChooses = computerChoices[Math.floor(Math.random() * wordChoices.length)];
+function losing(){
+	lettersAlreadyGuessed = [];
+	var guessedLetters = document.getElementById('lettersGuessed');
+    guessedLetters.textContent = lettersAlreadyGuessed;  
+
+	numGuesses = 15;
+	var guessesLeft = document.getElementById('guessesRemaining');
+    guessesLeft.textContent = numGuesses;
+
+	wordComputerChooses = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+	underscores = [];
+	for(var i = 0; i < wordComputerChooses.length; i++){
+    	underscores.push("_");
+    };
+	var wordUnderscores = document.getElementById('word');
+    wordUnderscores.textContent = underscores;  
 };
-/*
-on key up{
-	choose random array index;
-	i f the index is candyman (for instance)
-		put 8 underscores in html word area 
 
-	on key up next
-	if index string includes key pressed && numGuesses > 0
-		numGuesses--;
-		somehow add letter to the corresponding place in the underscores
-		add event.key push to lettersAlreadyGuessed;
-		display letter on page
-	else if index string includes key pressed && correct choices === word choices string
-		wins++;
-		numGuesses = 20;
-		lettersAlreadyGuessed = [];
-		choose random array index;
-		put the new number of underscores on the page (make fun,ction to do this?)
-	else if index string does not include key && numGuesses > 0
-		numGuesses--;
-		add event.key push to lettersAlreadyGuessed;
-		display letter on page;
-	else (when you lose bc numGuesses = 0)
-
-*/
-
-
+function giveUp(){
+	var givingUp = document.getElementById('word');
+    givingUp.textContent = wordComputerChooses; 
+}
